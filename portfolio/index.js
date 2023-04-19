4f9c1022792cd0f8318f5be871cd10fdfbb290bb
SlayzerZ <ibrahimallari84@gmail.com>
2023-04-19 15:30:15 +0200

    V1

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,21 +1,33 @@
 let Description = document.getElementsByClassName("Description")
-let Link = document.getElementsByClassName("Link")
-let Matchstick = new Image (800,500); Matchstick.src = '../assets/pictures/matchstick.jpg'
-let Hangman = new Image (900,500); Hangman.src = '../assets/pictures/hangman.gif'
+let Link = document.getElementById("Link")
+let space = "<br>"
+let ynovref = "https://github.com/SlayzerZ/Ynov-Rentiel"
+let Matchstick = new Image (1000,500); Matchstick.src = '../assets/pictures/matchstick.jpg'
+let Hangman = new Image (1000,500); Hangman.src = '../assets/pictures/hangman.gif'
 let HangmanWeb = new Image (1000,600); HangmanWeb.src = '../assets/pictures/hangman-web.gif'
-let Groupie1 = new Image (900,500); Groupie1.src = '../assets/pictures/groupie-tracker1.gif'
-let Groupie2 = new Image (900,500); Groupie2.src = '../assets/pictures/groupie-tracker2.gif'
-let Groupie3 = new Image (900,500); Groupie3.src = '../assets/pictures/groupie-tracker3.gif'
-let Sortable1 = new Image (900,500); Sortable1.src = '../assets/pictures/sortable1.gif'
-let Sortable2 = new Image (900,500); Sortable2.src = '../assets/pictures/sortable2.gif'
+let MusicShop = new Image (1000,600); MusicShop.src = '../assets/pictures/music-shop.gif'
+let Groupie1 = new Image (1000,500); Groupie1.src = '../assets/pictures/groupie-tracker1.gif'
+let Groupie2 = new Image (1000,500); Groupie2.src = '../assets/pictures/groupie-tracker2.gif'
+let Groupie3 = new Image (1000,500); Groupie3.src = '../assets/pictures/groupie-tracker3.gif'
+let GetThemAll = new Image (1000,600); GetThemAll.src = '../assets/pictures/get-them-all.gif'
+let BuildBrick = new Image (1000,600); BuildBrick.src = '../assets/pictures/brick.gif'
+let Pimp = new Image (1000,600); Pimp.src = '../assets/pictures/pimp.gif'
+let Keycodes = new Image (1000,600); Keycodes.src = '../assets/pictures/keycodes.gif'
+let Sortable1 = new Image (1000,600); Sortable1.src = '../assets/pictures/sortable1.gif'
+let Sortable2 = new Image (1000,600); Sortable2.src = '../assets/pictures/sortable2.gif'
 let Boxxle = new Image (1100,600); Boxxle.src = '../assets/pictures/boxxle.gif'
-console.log(Description)
 Description[0].appendChild(Matchstick)
 Description[1].appendChild(Hangman)
 Description[2].appendChild(HangmanWeb)
+Description[3].appendChild(MusicShop)
 Description[4].appendChild(Groupie1)
 Description[4].appendChild(Groupie2)
 Description[4].appendChild(Groupie3)
+Description[5].appendChild(GetThemAll)
+Description[6].appendChild(BuildBrick)
+Description[7].appendChild(Pimp)
+Description[8].appendChild(Keycodes)
 Description[9].appendChild(Sortable1)
 Description[9].appendChild(Sortable2)
 Description[10].appendChild(Boxxle)
+Link.innerHTML ="<b>" + "Lien des projets : " + "</b>" + space + "<a href=" +ynovref+ "> Ici</a>"
---
+++ portfolio/profil.js
@@ -0,0 +1,3 @@
+let Background = new Image (1100,1200); Background.src = '../assets/pictures/background.png'
+let Bg = new Image (1100,1200); Bg.src = '../assets/pictures/bg.jpg'
+document.body.style.backgroundImage = `url(${Bg.src})`
