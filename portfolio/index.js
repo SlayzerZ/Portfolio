1168a345e280f1fe632f3bd4c23f82150c16162e
SlayzerZ <ibrahimallari84@gmail.com>
2024-01-22 12:27:08 +0100

    New CV-

--- portfolio/index.js
+++ portfolio/index.js
@@ -6,33 +6,33 @@ let space = "<br>"
 let Separator = `<fieldset class="pose"></fieldset>`
 let Separator2 = `<hr class="pose"></hr>`
 let ynovref = "https://github.com/SlayzerZ/Ynov-Rentiel"
-let Matchstick = new Image (1000,500); Matchstick.src = './assets/pictures/matchstick.jpg'
-let Hangman = new Image (1000,500); Hangman.src = './assets/pictures/hangman.gif'
-let HangmanWeb = new Image (1000,600); HangmanWeb.src = './assets/pictures/hangman-web.gif'
-let MusicShop = new Image (1000,600); MusicShop.src = './assets/pictures/music-shop.gif'
-let Groupie1 = new Image (1000,500); Groupie1.src = './assets/pictures/groupie-tracker1.gif'
-let Groupie2 = new Image (1000,500); Groupie2.src = './assets/pictures/groupie-tracker2.gif'
-let Groupie3 = new Image (1000,500); Groupie3.src = './assets/pictures/groupie-tracker3.gif'
-let GetThemAll = new Image (1000,600); GetThemAll.src = './assets/pictures/get-them-all.gif'
-let BuildBrick = new Image (1000,600); BuildBrick.src = './assets/pictures/brick.gif'
-let Pimp = new Image (1000,600); Pimp.src = './assets/pictures/pimp.gif'
-let Keycodes = new Image (1000,600); Keycodes.src = './assets/pictures/keycodes.gif'
-let Sortable1 = new Image (1000,600); Sortable1.src = './assets/pictures/sortable1.gif'
-let Sortable2 = new Image (1000,600); Sortable2.src = './assets/pictures/sortable2.gif'
-let Boxxle = new Image (1100,600); Boxxle.src = './assets/pictures/boxxle.gif'
-let Simon = new Image (1100,600); Simon.src = './assets/pictures/simon.gif'
-let Forum1 = new Image (1000,600); Forum1.src = './assets/pictures/forum1.gif'
-let Forum2 = new Image (1000,600); Forum2.src = './assets/pictures/forum2.gif'
-let To_do1 = new Image (1000,600); To_do1.src = './assets/pictures/to-do1.gif'
-let To_do2 = new Image (1000,600); To_do2.src = './assets/pictures/to-do2.gif'
-let To_do3 = new Image (1000,600); To_do3.src = './assets/pictures/to-do3.gif'
-let To_do4 = new Image (1000,600); To_do4.src = './assets/pictures/to-do4.gif'
-let Tamago1 = new Image (800,1000); Tamago1.src = './assets/pictures/tamago1.gif'
-let Tamago2 = new Image (800,1000); Tamago2.src = './assets/pictures/tamago2.gif'
-let Tamago3 = new Image (800,1000); Tamago3.src = './assets/pictures/tamago3.gif'
-let Dactylo = new Image (1000,600); Dactylo.src = './assets/pictures/dactylo.gif'
-let Php1 = new Image (1000,600); Php1.src = './assets/pictures/php1.gif'
-let Php2 = new Image (1000,600); Php2.src = './assets/pictures/php2.gif'
+let Matchstick = new Image(1000, 500); Matchstick.src = './assets/pictures/matchstick.jpg'
+let Hangman = new Image(1000, 500); Hangman.src = './assets/pictures/hangman.gif'
+let HangmanWeb = new Image(1000, 600); HangmanWeb.src = './assets/pictures/hangman-web.gif'
+let MusicShop = new Image(1000, 600); MusicShop.src = './assets/pictures/music-shop.gif'
+let Groupie1 = new Image(1000, 500); Groupie1.src = './assets/pictures/groupie-tracker1.gif'
+let Groupie2 = new Image(1000, 500); Groupie2.src = './assets/pictures/groupie-tracker2.gif'
+let Groupie3 = new Image(1000, 500); Groupie3.src = './assets/pictures/groupie-tracker3.gif'
+let GetThemAll = new Image(1000, 600); GetThemAll.src = './assets/pictures/get-them-all.gif'
+let BuildBrick = new Image(1000, 600); BuildBrick.src = './assets/pictures/brick.gif'
+let Pimp = new Image(1000, 600); Pimp.src = './assets/pictures/pimp.gif'
+let Keycodes = new Image(1000, 600); Keycodes.src = './assets/pictures/keycodes.gif'
+let Sortable1 = new Image(1000, 600); Sortable1.src = './assets/pictures/sortable1.gif'
+let Sortable2 = new Image(1000, 600); Sortable2.src = './assets/pictures/sortable2.gif'
+let Boxxle = new Image(1100, 600); Boxxle.src = './assets/pictures/boxxle.gif'
+let Simon = new Image(1100, 600); Simon.src = './assets/pictures/simon.gif'
+let Forum1 = new Image(1000, 600); Forum1.src = './assets/pictures/forum1.gif'
+let Forum2 = new Image(1000, 600); Forum2.src = './assets/pictures/forum2.gif'
+let To_do1 = new Image(1000, 600); To_do1.src = './assets/pictures/to-do1.gif'
+let To_do2 = new Image(1000, 600); To_do2.src = './assets/pictures/to-do2.gif'
+let To_do3 = new Image(1000, 600); To_do3.src = './assets/pictures/to-do3.gif'
+let To_do4 = new Image(1000, 600); To_do4.src = './assets/pictures/to-do4.gif'
+let Tamago1 = new Image(800, 1000); Tamago1.src = './assets/pictures/tamago1.gif'
+let Tamago2 = new Image(800, 1000); Tamago2.src = './assets/pictures/tamago2.gif'
+let Tamago3 = new Image(800, 1000); Tamago3.src = './assets/pictures/tamago3.gif'
+let Dactylo = new Image(1000, 600); Dactylo.src = './assets/pictures/dactylo.gif'
+let Php1 = new Image(1000, 600); Php1.src = './assets/pictures/php1.gif'
+let Php2 = new Image(1000, 600); Php2.src = './assets/pictures/php2.gif'
 Description[0].appendChild(Matchstick)
 Description[1].appendChild(Hangman)
 Description[2].appendChild(HangmanWeb)
@@ -60,16 +60,16 @@ Description[15].appendChild(Forum1)
 Description[15].appendChild(Forum2)
 Description[16].appendChild(Php1)
 Description[16].appendChild(Php2)
-Link.innerHTML ="<b>" + "Lien des projets : " + "</b>" + space + "<a href=" +ynovref+ "> Ici</a>"
+Link.innerHTML = "<b>" + "Lien des projets : " + "</b>" + space + "<a href=" + ynovref + "> Ici</a>"
 
-let To_do = new Image (1000,600); To_do.src = './assets/pictures/to-do.gif'
-let DVD = new Image (1000,600); DVD.src = './assets/pictures/dvd.gif'
-let Currency = new Image (1000,600); Currency.src = './assets/pictures/currency.gif'
+let To_do = new Image(1000, 600); To_do.src = './assets/pictures/to-do.gif'
+let DVD = new Image(1000, 600); DVD.src = './assets/pictures/dvd.gif'
+let Currency = new Image(1000, 600); Currency.src = './assets/pictures/currency.gif'
 Description2[0].appendChild(Currency)
 Description2[1].appendChild(DVD)
 Description2[2].appendChild(To_do)
-//hey
+
 console.log(Description[14])
-for (i = 0; i < Projet.length; i++) { 
-    Projet[i].innerHTML = Projet[i].innerHTML + Separator2
-  }
+for (i = 0; i < Projet.length; i++) {
+  Projet[i].innerHTML = Projet[i].innerHTML + Separator2
+}
