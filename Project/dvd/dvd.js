746a1d48fc2240aaf23545316a64704aede00e4e
SlayzerZ <ibrahimallari84@gmail.com>
2024-09-05 16:10:46 +0200

    Paper Zelda

--- Project/dvd/dvd.js
+++ Project/dvd/dvd.js
@@ -1,41 +1,44 @@
 let x = 400;
 let y = 250;
+let speed = 1;
 let Top = false;
 let Left = false;
 let dvd = document.querySelectorAll('#dvd');
+
 function generateColor(){
     let color = '#'
     return color += Math.random().toString(16).slice(2, 8).toUpperCase();
 }
-function moov(){
+
+function move(){
     let height = window.innerHeight -200;
     let width = window.innerWidth -300;
-    if (y == height){
+    if (y >= height){
         Top = true;
         dvd[1].style.fill = generateColor();
-    } else if (y == -80){
+    } else if (y <= -80){
         Top = false;
         dvd[1].style.fill = generateColor();
     }
     if (Top){
-        y -= 1
+        y -= speed
     } else {
-        y += 1
+        y += speed
     }
-    if (x == width){
+    if (x >= width){
         Left = true;
         dvd[1].style.fill = generateColor();
-    } else if (x == 0){
+    } else if (x <= 0){
         Left = false;
         dvd[1].style.fill = generateColor();
     }
     if (Left){
-        x -= 1
+        x -= speed
     } else {
-        x += 1
+        x += speed
     }
     dvd[1].style.top = `${y}px`;
     dvd[1].style.left = `${x}px`;
-    window.requestAnimationFrame(moov)
+    window.requestAnimationFrame(move)
 }
-window.requestAnimationFrame(moov)
+window.requestAnimationFrame(move)
